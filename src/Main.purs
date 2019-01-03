208bc0a34fe77128a213132e4640fc4898c61ec4
Dylan Thinnes <dylan.thinnes@protonmail.com>
2019-01-03 13:45:37 +0000

    Basic implementation of JSON retrieval

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,17 @@
 module Main where
 
 import Prelude
-import Effect (Effect)
-import Effect.Console (log)
+import Effect
+import Effect.Class.Console
+import Affjax
+import Affjax.ResponseFormat
+import Effect.Aff
+import Data.Either
+import Data.Argonaut.Core
 
 main :: Effect Unit
-main = do
-  log "Hello sailor!"
+main = launchAff_ $ do
+    res1 <- get json "https://jsonplaceholder.typicode.com/todos/1"
+    case res1.body of
+        Left err -> log $ printResponseFormatError err
+        Right json -> log $ stringify json
